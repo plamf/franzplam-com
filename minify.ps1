e1fa75e68e3162482112c08c0af15429dc46ba3f
Franz Plam <franzl@live.at>
2023-01-28 19:21:51 +0100

    Add minification script

---
+++ minify.ps1
@@ -0,0 +1,1 @@
+html-minifier-terser --collapse-whitespace --input-dir "./" --output-dir "./" -o "index.html" --file-ext "html" --use-short-doctype --minify-css true --minify-js true --process-scripts true --remove-comments --remove-style-link-type-attributes --remove-tag-whitespace --remove-script-type-attributes
