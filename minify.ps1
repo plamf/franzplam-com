bda36b1f99abd25b8114e5d802bcaff744fe61c9
Franz Plam <franzl@live.at>
2023-01-28 19:40:03 +0100

    Move unminified file into separate directory

--- minify.ps1
+++ minify.ps1
@@ -1,1 +1,2 @@
-html-minifier-terser --collapse-whitespace --input-dir "./" --output-dir "./" -o "index.html" --file-ext "html" --use-short-doctype --minify-css true --minify-js true --process-scripts true --remove-comments --remove-style-link-type-attributes --remove-tag-whitespace --remove-script-type-attributes
+Copy-Item "./workingdir/unminified-index.html" "./index.html"
+html-minifier-terser --collapse-whitespace --input-dir "./" --output-dir "./" --file-ext "html" --use-short-doctype --minify-css true --minify-js true --process-scripts true --remove-comments --remove-style-link-type-attributes --remove-tag-whitespace --remove-script-type-attributes
